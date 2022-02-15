0adad302807ee464489dbd7afb525d3705c9bfad
CyberRobinMier <rmier@cyberdigma.nl>
2022-02-15 12:49:45 +0100

    added component AddTask, copied basic form structure. Will make dynamic and make it submit a new task. But first, lunch!

---
+++ src/components/AddTask.vue
@@ -0,0 +1,24 @@
+<!-- is going to be a form that upon submitting adds task to the list -->
+
+<template>
+<form class="add-form">
+    <div class="form-control">
+      <label>Task</label>
+      <input type="text" v-model="text" name="text" placeholder="Add Task" />
+    </div>
+    <div class="form-control">
+      <label>Day & Time</label>
+      <input
+        type="text"
+        name="day"
+        placeholder="Add Day & Time"
+      />
+    </div>
+    <div class="form-control form-control-check">
+      <label>Set Reminder</label>
+      <input type="checkbox" name="reminder" />
+    </div>
+    <input type="submit" value="Save Task" class="btn btn-block" />
+  </form>
+    
+</template>
